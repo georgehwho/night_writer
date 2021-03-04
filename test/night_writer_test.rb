e2b5d6a2fa28248295f9502d90308e8eb0f9367c
George Ho <george.h.w.ho@gmail.com>
2021-03-04 14:12:21 -0500

    Add skeleton

---
+++ test/night_writer_test.rb
@@ -0,0 +1,7 @@
+require_relative 'test_helper'
+
+class NightWriterTest < Minitest::Test
+  def test_it_exists
+    night_writer = NightWriter.new
+  end
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,8 @@
+require 'simplecov'
+SimpleCov.start do
+end
+
+require 'minitest/autorun'
+require 'mocha/minitest'
+
+require './lib/night_writer'
