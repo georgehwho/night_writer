7f050865bbfe843864a79a8b4f6437e1534ea2b3
George Ho <george.h.w.ho@gmail.com>
2021-03-05 15:38:58 -0500

    Add braille alphabet module and test

--- lib/write_braille.rb
+++ lib/write_braille.rb
@@ -1,9 +1,17 @@
+require_relative 'braille'
+
 class WriteBraille
+  include Braille
+
   attr_reader :file_path,
-              :night_writer
+              :night_writer,
+              :file
 
   def initialize(file_path, night_writer)
     @file_path = file_path
     @night_writer = night_writer
+    @file = night_writer.reader_contents
   end
+
+
 end
--- test/write_braille_test.rb
+++ test/write_braille_test.rb
@@ -3,9 +3,26 @@ require_relative 'test_helper'
 class WriteBrailleTest < Minitest::Test
   def test_it_exists
     night_writer = mock
+    night_writer.stubs(:reader_contents).returns('hello')
     writer = WriteBraille.new('braille.txt', night_writer)
+
     assert_instance_of WriteBraille, writer
   end
 
+  def test_it_has_readable_attributes
+    night_writer = mock
+    night_writer.stubs(:reader_contents).returns('hello')
+    writer = WriteBraille.new('braille.txt', night_writer)
 
+    assert_equal 'braille.txt', writer.file_path
+    assert_equal 'hello', writer.file
+  end
+
+  def test_braille_module_works
+    night_writer = mock
+    night_writer.stubs(:reader_contents).returns('hello')
+    writer = WriteBraille.new('braille.txt', night_writer)
+
+    assert_equal [[0, "."], [".", "."], [".", "."]], writer.to_braille['a']
+  end
 end
