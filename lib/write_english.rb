199fd99144c5c317074fafb6ddae0ba2aa0f3635
George Ho <george.h.w.ho@gmail.com>
2021-03-06 16:52:07 -0500

    Add file to rows

--- lib/write_english.rb
+++ lib/write_english.rb
@@ -1,4 +1,8 @@
+require_relative 'braille'
+
 class WriteEnglish
+  include Braille
+
   attr_reader :file_path,
               :night_reader,
               :file
@@ -8,5 +12,18 @@ class WriteEnglish
     @night_reader = night_reader
     @file = night_reader.reader_contents
     # write_file(sanitize_file(file))
+  end
+
+  def write_file(input) ### get back to this
+    return 'no file path given' if file_path.nil?
+    File.delete(file_path) if File.exist?(file_path)
+    input.each do |row|
+      File.open(file_path, "a") { |f| f.puts "#{row.join}" }
+    end
+  end
+
+  def file_to_rows(string = file)
+    chars = string.delete("\n").split('')
+    chars.each_slice(chars.size/3).to_a
   end
 end
--- test/write_english_test.rb
+++ test/write_english_test.rb
@@ -2,5 +2,49 @@ require_relative 'test_helper'
 require './lib/write_english'
 
 class WriteEnglishTest < Minitest::Test
+  def test_it_exists
+    night_reader = mock
+    night_reader.stubs(:reader_contents).returns("0.\n00\n..\n") # this is h
 
+    writer = WriteEnglish.new('original_message.txt', night_reader)
+
+    assert_instance_of WriteEnglish, writer
+  end
+
+  def test_it_has_readable_attributes
+    night_reader = mock
+    night_reader.stubs(:reader_contents).returns("0.\n00\n..\n") # this is h
+
+    writer = WriteEnglish.new('original_message.txt', night_reader)
+
+    assert_equal 'original_message.txt', writer.file_path
+    assert_equal "0.\n00\n..\n", writer.file
+  end
+
+  def test_it_can_write_to_a_file ## get back to this
+    skip
+    night_reader = mock
+    night_reader.stubs(:reader_contents).returns("0.\n00\n..\n") # this is h
+    writer = WriteEnglish.new('original_message.txt', night_reader)
+
+    writer.write_file([[0, "."], [".", "."], [".", "."]])
+
+    assert_equal "0.\n..\n..\n", File.read('braille.txt')
+  end
+
+  def test_it_can_convert_to_english
+    night_reader = mock
+    night_reader.stubs(:reader_contents).returns("0.\n00\n..\n") # this is h
+    writer = WriteEnglish.new('original_message.txt', night_reader)
+
+    assert_equal 'h', writer.to_english[[['0', '.'], ['0', '0'], ['.', '.']]]
+  end
+
+  def test_it_can_go_from_file_to_rows
+    night_reader = mock
+    night_reader.stubs(:reader_contents).returns("0.\n00\n..\n") # this is h
+    writer = WriteEnglish.new('original_message.txt', night_reader)
+
+    assert_equal [["0", '.'], ["0", "0"], ['.', '.']], writer.file_to_rows("0.\n00\n..\n")
+  end
 end
